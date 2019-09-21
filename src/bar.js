5f38984ca04aa3c768145789aff4a8d48c36dc4f
iwtn <yasuaki.iwata@livesense.co.jp>
2019-09-21 16:48:03 +0900

    prototype

--- src/bar.js
+++ src/bar.js
@@ -1,3 +1,6 @@
 export default function bar() {
-  console.log('Hello, Google Calendar Selector.');
+  const sideBar = document.querySelector('[jsname="TZVvfc"]');
+  sideBar.scrollBy(0, 100);
+  const checkBoxes = document.querySelectorAll('[role="checkbox"][aria-checked="false"]');
+  console.log(checkBoxes);
 }
